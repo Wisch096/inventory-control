2952843d597eb63b776aedd9cead083bebb9e005
Matheus Wisch <wisch.pg@outlook.com>
2024-05-15 20:41:46 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet]
+    imports: [
+      RouterOutlet,
+      RouterLink
+    ]
 })
 export class AppComponent {
   title = 'inventory-control';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { StockControlComponent } from './pages/stock-control/stock-control.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'stock-control',
+        component: StockControlComponent
+    }
+];
