95f0a6e285a92d32a3c4fb30abd557798a763921
Matheus Wisch <wisch.pg@outlook.com>
2024-05-15 19:33:28 -0300

    feat: home & stock control

---
+++ src/app/pages/stock-control/stock-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-control.component.html',
+  styleUrl: './stock-control.component.css'
+})
+export class StockControlComponent {
+
+}
