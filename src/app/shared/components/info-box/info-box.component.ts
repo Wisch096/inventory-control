e5fdb63b8e6aa43e9ca638e26536cdd6773acc3c
Matheus Wisch <wisch.pg@outlook.com>
2024-05-18 15:00:36 -0300

    feat: add icon prop

--- src/app/shared/components/info-box/info-box.component.ts
+++ src/app/shared/components/info-box/info-box.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-info-box',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+  ],
   templateUrl: './info-box.component.html',
   styleUrl: './info-box.component.css'
 })
 export class InfoBoxComponent {
-  @Input() icon!: string;
+  @Input() icon!: IconProp;
   @Input() title!: string;
   @Input() quantity!: number;
 }
